e28c8e4a2e9b1000aee6488ed869f295eec4b75c
joycytao <ctao01.poly@gmail.com>
2012-12-20 10:23:23 -0600

    Initial Commit

---
+++ HeaderForm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HeaderForm
+//
+//  Created by Joy Tao on 12/20/12.
+//  Copyright (c) 2012 Joy Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
